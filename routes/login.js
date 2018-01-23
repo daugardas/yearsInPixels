dc5d9541c72cfa4e434e8d6af5ff005cb2507498
daugardas <daugismokykla@gmail.com>
2018-01-23 20:45:54 +0200

    Made login/errors popup in forms, not in a redirect

--- routes/login.js
+++ routes/login.js
@@ -12,8 +12,14 @@ router.post(`/`, (req,res, next)=>{
     if(req.body.username && req.body.password){
         User.authenticate(req.body.username, req.body.password, (error, user)=>{
           if(error || !user){
-            var err = new Error('Wrong email or password');
-            err.status = 401;
+            let err;
+            if(!(error === undefined) && error.message === `User not found`){
+              err = new Error('User not found');
+              err.status = 401;
+            } else {
+              err = new Error('Wrong password');
+              err.status = 401;
+            }
             return next(err);
           } else {
             req.session.userId = user._id;
@@ -21,10 +27,21 @@ router.post(`/`, (req,res, next)=>{
           }
         });
     } else {
-        var err = new Error('All fields required');
+        let err = new Error('All fields required');
         err.status = 400;
         return next(err);
     }
+});
+// login had an error, so rerender the login form with errors
+router.use(function(err, req, res, next) {
+  if(err.message === `User not found`){
+    return res.render('index', {userErr: err.message})
+  } else if (err.message === `Wrong password`){
+    return res.render('index', {passErr: err.message})
+  } else if (err.message === `All fields required`){
+    return res.render('index', {logFieldsErr: err.message});
+  }
+  return next(err);
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -13,7 +13,7 @@ router.post(`/`, (req,res, next)=>{
     if(req.body.email && req.body.username && req.body.password && req.body.passwordConf){
 
         if(req.body.password !== req.body.passwordConf){
-            var err = new Error('Passwords do not match.');
+            var err = new Error('Passwords do not match');
             err.status = 400;
             return next(err);
         };
@@ -30,17 +30,40 @@ router.post(`/`, (req,res, next)=>{
                 return next(err);
             }
             req.session.userId = user._id;
+            Emotion.create({username: req.body.username},(err, emotion)=>{
+              if(err) return next(err);
+            });
             return res.redirect(`/`);
         });
 
-        Emotion.create({username: req.body.username},(err, emotion)=>{
-            if(err) return next(err);
-        });
+        
     } else {
-        var err = new Error('All fields required');
+        let err = new Error('All fields required');
         err.status = 400;
         return next(err);
     }
+});
+
+router.use((err,req,res,next)=>{
+    // username or email already exists
+    if(err.code === 11000){
+      
+      let field = err.message.split('.$')[1];
+      // now we have `<username || email>_1 dup key`
+      field = field.split(' dup key')[0];
+      field = field.substring(0, field.lastIndexOf('_')); // returns username || email
+      console.log(field);
+      if(field === `username`){
+        return res.render('index', {regUserErr: "Username already exists"});
+      }else if (field === 'email'){
+        return res.render('index', {regEmailErr: "There is already an user with this email"});
+      }
+    }else if(err.message === `All fields required`){
+      return res.render('index', {regFieldsErr: err.message});
+    } else if(err.message === `Passwords do not match`){
+      return res.render('index', {regPassMatchErr: err.message});
+    }
+    return next(err);
 });
 
 module.exports = router;
